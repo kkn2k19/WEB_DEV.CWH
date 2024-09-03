8d54c9d3d5264cf38fd8b42df00d3389edb24bc4
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-09-03 14:43:08 +0530

    Exercise 12 (Color the boxes)

---
+++ Video070/script.js
@@ -0,0 +1,22 @@
+let boxes = document.querySelector(".container").children
+
+/*
+range -- 0 to 255
+Math.random -- 0 to 1 with 1 exclusive 
+so multiplied with 255
+and and added the starting point to show from where it is starting.
+
+after that the ceiling value of the number is been put in rgb() function 
+*/
+
+function getRandomColor(){
+    let v1 = Math.ceil(0 + Math.random()*255);
+    let v2 = Math.ceil(0 + Math.random()*255);
+    let v3 = Math.ceil(0 + Math.random()*255);
+
+    return `rgb(${v1}, ${v2}, ${v3})`;
+}
+
+Array.from(boxes).forEach(e => {
+    e.style.backgroundColor = getRandomColor();
+});

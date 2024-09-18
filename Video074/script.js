9d1118ce16d73d599aeddb07b5dc9a1371966b7c
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2024-09-19 00:02:45 +0530

    Create script.js

---
+++ Video074/script.js
@@ -0,0 +1,34 @@
+let button = document.getElementById("btn");
+
+// List of all mouse events
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+// button.addEventListener("click", ()=>{
+//     alert("I was clicked. Yayy!");
+// })
+
+// button.addEventListener("click", ()=>{
+//     document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!";
+// });
+
+// button.addEventListener("dblclick", ()=>{
+//     document.querySelector(".box").innerHTML = "Double Clicked";
+// });
+
+// button.addEventListener("contextmenu", ()=>{
+//     alert("Dont hack us by Right click please.");
+// });
+
+// document.addEventListener("keydown", (e) => {
+//     console.log(e.key, e.keyCode);
+// });
+
+// k 75
+// a 65
+// r 82
+// a 65
+// n 78
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.key, e.keyCode);
+});
